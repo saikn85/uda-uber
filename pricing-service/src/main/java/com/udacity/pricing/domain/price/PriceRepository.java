e8a54111b34ddd5423604b8190e5c4125bafa636
saikn85 <85madmaxfury@gmail.com>
2023-10-17 23:11:49 +0530

    cleand soln

--- pricing-service/src/main/java/com/udacity/pricing/domain/price/PriceRepository.java
+++
@@ -1,9 +0,0 @@
-package com.udacity.pricing.domain.price;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.rest.core.annotation.RepositoryRestResource;
-
-@RepositoryRestResource(collectionResourceRel = "prices", path = "prices")
-public interface PriceRepository extends JpaRepository<Price, Long> {
-    Price findByVehicleId(Long vehicleId);
-}
